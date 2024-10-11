8ce03573c26cb62f09aabd8995d44c978ae299d3
Jakob Koller <jaki.koller@gmail.com>
2024-10-11 13:39:27 +0200

    Tracking nanoTime

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,13 @@ public class Main {
     public static void main(String[] args) {
         String start = "Rönerweg/1";
         String end = "Schänzlihalde/1";
-        BreadthFirst breadthFirst = new BreadthFirst(start, end);
-
-        DepthFirst depth = new DepthFirst(start, end);
-
         //BreadthFirst breadthFirst = new BreadthFirst(start, end);
+        //DepthFirst depth = new DepthFirst(start, end);
         //BestFirst bestFirst = new BestFirst(start, end);
+        Long startTime = System.nanoTime();
         AStar aStar = new AStar(start, end);
+        Long endTime = System.nanoTime();
+        double result = endTime - startTime;
+        System.out.println(result / 1000000000);
     }
 }
