6638cba1611d244d2a0ae0f706c767aa112c4722
Lehkdup <s.lehkdup@gmail.com>
2024-10-11 13:05:34 +0200

    DeptFirst implementiert

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import MapData.MapData;
 import breadthFirst.BreadthFirst;
+import depthFirst.DepthFirst;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         String start = "Neuengasse/1";
         String end = "Schänzlihalde/1";
         BreadthFirst breadthFirst = new BreadthFirst(start, end);
+
+        DepthFirst depth = new DepthFirst(start, end);
     }
 }
