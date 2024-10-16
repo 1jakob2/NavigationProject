74037cd11eb4284af1caae971d00b5a0abd04a0a
Lehkdup <s.lehkdup@gmail.com>
2024-10-16 15:24:36 +0200

    zusätzliche mapdaten hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import aStar.AStar;
 import bestFirst.BestFirst;
 import breadthFirst.BreadthFirst;
 import depthFirst.DepthFirst;
-import dijkstra.Dijkstra;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,14 +13,12 @@ public class Main {
     public static void main(String[] args) {
         String start = "Rönerweg/1";
         String end = "Schänzlihalde/1";
+        BreadthFirst breadthFirst = new BreadthFirst(start, end);
+
+        DepthFirst depth = new DepthFirst(start, end);
+
         //BreadthFirst breadthFirst = new BreadthFirst(start, end);
-        //DepthFirst depth = new DepthFirst(start, end);
         //BestFirst bestFirst = new BestFirst(start, end);
-//        Long startTime = System.nanoTime();
-//        AStar aStar = new AStar(start, end);
-//        Long endTime = System.nanoTime();
-//        double result = endTime - startTime;
-//        System.out.println(result / 1000000000);
-        Dijkstra dijkstra = new Dijkstra(start, end);
+        AStar aStar = new AStar(start, end);
     }
 }
